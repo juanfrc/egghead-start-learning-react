6150ccec6c0440ef01ed778aa2cb6e7975c4fa5a
juanfrc <juanfrc2@gmail.com>
2018-07-14 18:59:06 -0500

    video 2 class and stateless component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import React from 'react';
 
-const App = () => <h1>Hello Juan</h1>
+
+// Create as a class
+// class App extends React.Component {
+//   render() {
+//     return <h1>Hello World</h1>
+//   }
+// }
+
+// Create as a stateless component
+const App = () => <h1>Hello stateless</h1>
+
 
 export default App
