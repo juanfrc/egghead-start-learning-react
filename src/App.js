5943d8c559769e3b57ff212898fa5b881f225408
juanfrc <juanfrc2@gmail.com>
2018-07-14 21:35:51 -0500

    video 7 nested data with props.children

--- src/App.js
+++ src/App.js
@@ -1,46 +1,63 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
 // Create as a stateless component
 // const App = () => <h1>Hello stateless</h1>
 
 // Create as a class
+
+// Commented on video 6
+// class App extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       txt: 'this is the state txt',
+//       cat: 0
+//     }
+//   }
+//
+//   update (e) {
+//     this.setState (
+//       { txt: e.target.value }
+//     )
+//   }
+//
+//   render() {
+//     return (
+//       <div>
+//         <h1>{this.state.txt} - {this.state.cat}</h1>
+//         <Widget update={this.update.bind(this)} />
+//       </div>
+//     )
+//   }
+// }
+//
+// const Widget = (props) =>
+//   <input type="text" onChange={props.update} />
+//
+// App.propTypes = {
+//   txt: PropTypes.string,
+//   cat: PropTypes.number.isRequired
+// }
+//
+// App.defaultProps = {
+//   txt: "this is the default txt"
+// }
+
+
+// Video 6 props.children
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      txt: 'this is the state txt',
-      cat: 0
-    }
-  }
-
-  update (e) {
-    this.setState (
-      { txt: e.target.value }
-    )
-  }
-
   render() {
-    return (
-      <div>
-        <h1>{this.state.txt} - {this.state.cat}</h1>
-        <Widget update={this.update.bind(this)} />
-      </div>
-    )
+    return <Button>I <Heart /> React</Button>
   }
 }
 
-const Widget = (props) =>
-  <input type="text" onChange={props.update} />
+const Button = (props) => <button>{props.children}</button>
 
-App.propTypes = {
-  txt: PropTypes.string,
-  cat: PropTypes.number.isRequired
-}
-
-App.defaultProps = {
-  txt: "this is the default txt"
-  // cat: PropTypes.number.isRequired
+class Heart extends React.Component {
+  render() {
+    return <span>&hearts;</span>
+  }
 }
 
 export default App
