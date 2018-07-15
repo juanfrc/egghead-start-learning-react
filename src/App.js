8489250058218bdfcc6f8d0370763d32b66240e0
juanfrc <juanfrc2@gmail.com>
2018-07-14 19:01:57 -0500

    video 3 wrap components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 
-
 // Create as a class
-// class App extends React.Component {
-//   render() {
-//     return <h1>Hello World</h1>
-//   }
-// }
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <b>Bold</b>
+      </div>
+    )
+  }
+}
 
 // Create as a stateless component
-const App = () => <h1>Hello stateless</h1>
-
+// const App = () => <h1>Hello stateless</h1>
 
 export default App
