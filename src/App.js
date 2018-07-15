eb644026806b9493a898fed731e2df7feb79fe8e
juanfrc <juanfrc2@gmail.com>
2018-07-14 21:28:59 -0500

    video 6 react components as children

--- src/App.js
+++ src/App.js
@@ -23,12 +23,15 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <input type="text" onChange={this.update.bind(this)} />
         <h1>{this.state.txt} - {this.state.cat}</h1>
+        <Widget update={this.update.bind(this)} />
       </div>
     )
   }
 }
+
+const Widget = (props) =>
+  <input type="text" onChange={props.update} />
 
 App.propTypes = {
   txt: PropTypes.string,
