32b8f76704672630937cc5f9397b19bd5438865e
juanfrc <juanfrc2@gmail.com>
2018-07-14 21:58:14 -0500

    video 9 synthetic events

--- src/App.js
+++ src/App.js
@@ -61,22 +61,51 @@ import React from 'react';
 // }
 
 // Video 8
+// class App extends React.Component {
+//   render() {
+//     return <Title text="The text" />
+//   }
+// }
+//
+// const Title = (props) => <h1>Title: {props.text}</h1>
+//
+// Title.propTypes = {
+//   text(props, propName, component) {
+//     if (!(propName in props)) {
+//       return new Error(`missing ${propName}`)
+//     }
+//     if (props[propName].length < 6) {
+//       return new Error(`${propName} was to short`)
+//     }
+//   }
+// }
+
+// Video 9 Synthetic event system
 class App extends React.Component {
-  render() {
-    return <Title text="The text" />
+  constructor () {
+    super();
+    this.state = {currentEvent: '-------'}
+    this.update = this.update.bind(this)
   }
-}
 
-const Title = (props) => <h1>Title: {props.text}</h1>
+  update(e) {
+    this.setState({currentEvent: e.type})
+  }
 
-Title.propTypes = {
-  text(props, propName, component) {
-    if (!(propName in props)) {
-      return new Error(`missing ${propName}`)
-    }
-    if (props[propName].length < 6) {
-      return new Error(`${propName} was to short`)
-    }
+  render () {
+    return (
+      <div>
+        <textarea
+          onKeyPress={this.update}
+          onCopy={this.update}
+          onCut={this.update}
+          onPaste={this.update}
+          onFocus={this.update}
+          onBlur={this.update}
+          cols="30" rows="10" />
+        <h1>{this.state.currentEvent}</h1>
+      </div>
+    )
   }
 }
 
