4477c9a7af2753399622ec83ddf2856c1380628a
juanfrc <juanfrc2@gmail.com>
2018-07-16 20:56:30 -0500

    Video 18 React.Children utilities

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import 'App.css'
+import './App.css'
 // import ReactDOM from 'react-dom';
 // import PropTypes from 'prop-types';
 
@@ -367,44 +367,74 @@ import 'App.css'
 // export default App
 
 // video 16 Live compiler as react component
+// class App extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       input: '/* Add your jsx here */',
+//       output: '',
+//       err: ''
+//     }
+//   }
+//
+//   update(e) {
+//     let code = e.target.value;
+//     try {
+//       this.setState({
+//         output: window.Babel
+//           .transform(code, { presets: ['es2015', 'react']})
+//           .code,
+//         err: ''
+//       })
+//     } catch (err) {
+//       this.setState({err: err.message})
+//     }
+//   }
+//
+//   render() {
+//     return (
+//       <div>
+//         <header>{this.state.err}</header>
+//         <div className="container">
+//           <textarea
+//             onChange={this.update.bind(this)}
+//             defaultValue={this.state.input} />
+//           <pre>
+//             {this.state.output}
+//           </pre>
+//         </div>
+//       </div>
+//     )
+//   }
+// }
+//
+// export default App
+
+// Video 18 React children utilities
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      input: '/* Add your jsx here */',
-      output: '',
-      err: ''
-    }
-  }
-
-  update(e) {
-    let code = e.target.value;
-    try {
-      this.setState({
-        output: window.Babel
-          .transform(code, { presets: ['es2015', 'react']})
-          .code,
-        err: ''
-      })
-    } catch (err) {
-      this.setState({err: err.message})
-    }
-  }
-
   render() {
     return (
-      <div>
-        <header>{this.state.err}</header>
-        <div className="container">
-          <textarea
-            onChange={this.update.bind(this)}
-            defaultValue={this.state.input} />
-          <pre>
-            {this.state.output}
-          </pre>
-        </div>
-      </div>
+      <Parent>
+        <div className="childA"></div>
+        <div className="childB"></div>
+      </Parent>
     )
+  }
+}
+
+class Parent extends React.Component {
+  render() {
+    console.log(this.props.children)
+    // let items = React.Children
+    //   .map(this.props.childred, child => child)
+
+    // let items = React.Children.toArray(this.props.children)
+
+    let items = React.Children.forEach(this.props.children,
+      child => console.log(this.props.className))
+
+    console.log(items)
+    return null
   }
 }
 
